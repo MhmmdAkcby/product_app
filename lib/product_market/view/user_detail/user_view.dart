13bfe99342d1be6b057296c5f93ae925f12a642e
MhmmdAkcby <muhammedakcabey546@gmail.com>
2024-08-27 18:33:50 +0300

    user detail ui created

--- lib/product_market/view/user_detail/user_view.dart
+++ lib/product_market/view/user_detail/user_view.dart
@@ -18,6 +18,7 @@ class UserView extends StatefulWidget {
 class _UserViewState extends State<UserView> {
   late NavigationService navigationService;
   final GetIt _getIt = GetIt.instance;
+  final String routePath = '/login';
 
   @override
   void initState() {
@@ -30,16 +31,79 @@ class _UserViewState extends State<UserView> {
     final cubit = context.read<LoginCubit>();
 
     return Scaffold(
-      appBar: AppBar(),
-      body: _buildUI(context, cubit),
+      body: _buildUI(context: context, cubit: cubit),
     );
   }
 
-  Column _buildUI(BuildContext context, LoginCubit cubit) {
+  Widget _buildUI({required BuildContext context, required LoginCubit cubit}) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _userDetails(context: context, cubit: cubit),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _userDetails({required BuildContext context, required LoginCubit cubit}) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        _userProfileView(),
+        _user(text: 'UserName', icon: Icons.supervised_user_circle_rounded),
+        _user(text: 'UserSurname', icon: Icons.supervised_user_circle_rounded),
+        _user(text: 'email', icon: Icons.email),
+        _user(text: 'country', icon: Icons.location_on),
+        _user(text: 'country', icon: Icons.location_on),
         _logoutButton(context, cubit),
       ],
+    );
+  }
+
+  Widget _user({required String text, required IconData icon}) {
+    return Container(
+      margin: const _WidgetPaddingAndMargin.marginAll(),
+      padding: const _WidgetPaddingAndMargin.paddingAll(),
+      width: MediaQuery.of(context).size.width * _WidgetSize().userWidth,
+      height: MediaQuery.of(context).size.height * _WidgetSize().userHeight,
+      decoration: _userBoxDecoration(),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text(text), Icon(icon, color: ProjectColor.flushOrange())]),
+    );
+  }
+
+  BoxDecoration _userBoxDecoration() {
+    return BoxDecoration(
+      color: ProjectColor.whiteColor(),
+      borderRadius: BorderRadius.circular(_WidgetSize().borderRadius),
+      boxShadow: [_userBoxShadow()],
+    );
+  }
+
+  BoxShadow _userBoxShadow() {
+    return BoxShadow(
+      color: ProjectColor.mortarGrey(),
+      blurRadius: _WidgetSize().blurRadius,
+      spreadRadius: _WidgetSize().spreadRadius,
+      offset: Offset(_WidgetSize().offsetX, _WidgetSize().offsetY),
+    );
+  }
+
+  Widget _userProfileView() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * _WidgetSize().userProfileWidth,
+      height: MediaQuery.of(context).size.height * _WidgetSize().userProfileWidth,
+      child: CircleAvatar(
+          backgroundColor: ProjectColor.flushOrange(),
+          child: Icon(Icons.person, size: _WidgetSize().circularAvatarIcon, color: ProjectColor.whiteColor())),
     );
   }
 
@@ -68,10 +132,10 @@ class _UserViewState extends State<UserView> {
   Future<void> _logoutMethod(bool isLoading, LoginCubit cubit) async {
     if (!isLoading) {
       await cubit.logout();
-      await Future.delayed(const Duration(milliseconds: 100));
+      await Future.delayed(Duration(milliseconds: _WidgetSize().milliseconds));
       if (!cubit.state.isLoading) {
         setState(() {});
-        navigationService.pushNamedAndRemoveUntil('/login');
+        navigationService.pushNamedAndRemoveUntil(routePath);
       }
     }
   }
@@ -79,10 +143,21 @@ class _UserViewState extends State<UserView> {
 
 class _WidgetSize {
   final double myButtonHeight = 0.07;
-  final double myButtonWidth = 0.3;
+  final double myButtonWidth = 0.8;
   final double myButtonFontSize = 17;
   final double borderRadius = 10;
   final double buttonFontSize = 18;
+  final double userWidth = 0.8;
+  final double userHeight = 0.07;
+  final double blurRadius = 5;
+  final double spreadRadius = 3;
+  final double offsetX = 0;
+  final double offsetY = 2;
+  final double userProfileWidth = 0.5;
+  final double userProfileHeight = 0.2;
+  final double circularAvatarIcon = 70;
+
+  final int milliseconds = 100;
 }
 
 class _WidgetPaddingAndMargin extends EdgeInsets {
